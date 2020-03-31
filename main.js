52da15738eb13438de0addb6f73e5a5e2131b500
xtha1 <shankarxtha@gmail.com>
2020-03-31 22:53:20 +0545

    for learning purpose

---
+++ main.js
@@ -0,0 +1,9 @@
+function initMap()
+{
+    var display={
+         zoom:5,
+         center:{lat:28.3949,lng:84.1240}
+
+    } 
+    var map=new google.maps.Map(document.getElementById('map'),display);
+}
